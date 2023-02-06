eae9257c1c123247e855251e68af84921fb4ec55
Yan Reginevich <83975655+BonGeras@users.noreply.github.com>
2023-02-06 15:29:07 +0100

    Initial Stage

---
+++ PhotoApp/ApiUsers/src/main/java/com/example/apiusers/controller/UsersController.java
@@ -0,0 +1,14 @@
+package com.example.apiusers.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UsersController {
+    @GetMapping("/status/check")
+    public String status() {
+        return "User Service working";
+    }
+}
