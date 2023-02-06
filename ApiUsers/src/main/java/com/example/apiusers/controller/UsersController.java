4c40b928e1c0ddb20c363e860985191fc92b64a6
Yan Reginevich <83975655+BonGeras@users.noreply.github.com>
2023-02-06 19:07:29 +0100

    Load Balancer in action

--- ApiUsers/src/main/java/com/example/apiusers/controller/UsersController.java
+++ ApiUsers/src/main/java/com/example/apiusers/controller/UsersController.java
@@ -1,5 +1,7 @@
 package com.example.apiusers.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +9,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/users")
 public class UsersController {
+
+    @Autowired
+    private Environment environment;
+
     @GetMapping("/status/check")
     public String status() {
-        return "User Service working";
+        return "User Service working on port " + environment.getProperty("local.server.port");
     }
 }
