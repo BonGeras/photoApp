af0c6459879f2ce753460ec78602f18db595acc6
Yan Reginevich <83975655+BonGeras@users.noreply.github.com>
2023-02-07 19:06:39 +0100

    Added some mods and features

--- PhotoApp/ApiUsers/src/main/java/com/example/apiusers/controller/UsersController.java
+++ PhotoApp/ApiUsers/src/main/java/com/example/apiusers/controller/UsersController.java
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream:PhotoApp/ApiUsers/src/main/java/com/example/apiusers/controller/UsersController.java
 package com.example.apiusers.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,3 +13,30 @@ public class UsersController {
         return "User Service working";
     }
 }
+=======
+package com.example.apiusers.controller;
+
+import com.example.apiusers.model.CreateUserRequestModel;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/users")
+public class UsersController {
+
+    @Autowired
+    private Environment environment;
+
+    @GetMapping("/status/check")
+    public String status() {
+        return "User Service working on port " + environment.getProperty("local.server.port");
+    }
+
+    @PostMapping
+    public String createUser(@Valid @RequestBody CreateUserRequestModel userDetails) {
+        return "create method is called";
+    }
+}
+>>>>>>> Stashed changes:ApiUsers/src/main/java/com/example/apiusers/controller/UsersController.java
