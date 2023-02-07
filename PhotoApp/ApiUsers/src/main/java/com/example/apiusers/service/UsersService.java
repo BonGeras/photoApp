34a4625b38ce5c3fecb4a5f1f0b0dcea8bd9cd52
Yan Reginevich <83975655+BonGeras@users.noreply.github.com>
2023-02-07 20:48:10 +0100

    Create a shared DTO class

---
+++ PhotoApp/ApiUsers/src/main/java/com/example/apiusers/service/UsersService.java
@@ -0,0 +1,7 @@
+package com.example.apiusers.service;
+
+import com.example.apiusers.shared.UserDTO;
+
+public interface UsersService {
+    UserDTO createUser(UserDTO userDetails);
+}
---
+++ PhotoApp/ApiUsers/src/main/java/com/example/apiusers/service/UsersServiceImpl.java
@@ -0,0 +1,10 @@
+package com.example.apiusers.service;
+
+import com.example.apiusers.shared.UserDTO;
+
+public class UsersServiceImpl implements UsersService {
+    @Override
+    public UserDTO createUser(UserDTO userDetails) {
+        return null;
+    }
+}
