c443a5e54b412778a842fdf1efdfb07545480e2f
Yan Reginevich <83975655+BonGeras@users.noreply.github.com>
2023-02-07 20:54:30 +0100

    Generate unique public user id

--- PhotoApp/ApiUsers/src/main/java/com/example/apiusers/service/UsersServiceImpl.java
+++ PhotoApp/ApiUsers/src/main/java/com/example/apiusers/service/UsersServiceImpl.java
@@ -2,9 +2,12 @@ package com.example.apiusers.service;
 
 import com.example.apiusers.shared.UserDTO;
 
+import java.util.UUID;
+
 public class UsersServiceImpl implements UsersService {
     @Override
     public UserDTO createUser(UserDTO userDetails) {
+        userDetails.setUserId(UUID.randomUUID().toString());
         return null;
     }
 }
