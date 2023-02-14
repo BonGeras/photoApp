afaf6b860895cb987d96239cf301cd40d88b3074
Yan Reginevich <83975655+BonGeras@users.noreply.github.com>
2023-02-14 16:32:07 +0100

    AuthenticationFilter.java has been added

--- PhotoApp/ApiUsers/src/main/java/com/example/apiusers/security/WebSecurity.java
+++ PhotoApp/ApiUsers/src/main/java/com/example/apiusers/security/WebSecurity.java
@@ -1,36 +1,34 @@
 package com.example.apiusers.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.access.expression.WebExpressionAuthorizationManager;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 @EnableWebSecurity
-public class WebSecurity {
+public class WebSecurity extends WebSecurityConfigurerAdapter {
     private Environment environment;
 
     @Autowired
     public WebSecurity(Environment environment) {
         this.environment = environment;
     }
-    @Bean
-    protected SecurityFilterChain configure(HttpSecurity http) throws Exception {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
-        http.authorizeHttpRequests()
-                .requestMatchers(HttpMethod.POST, "/users").access(
-                        new WebExpressionAuthorizationManager("hasIpAddress('"+environment.getProperty("gateway.ip")+"')"))
-                .requestMatchers(new AntPathRequestMatcher("/h2-console/**")).permitAll()
+        http.authorizeRequests().antMatchers("/**").hasIpAddress(environment.getProperty("gateway.ip"))
                 .and()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+                .addFilter(getAuthenticationFilter());
         http.headers().frameOptions().disable();
-        return http.build();
+    }
+
+    private AuthenticationFilter getAuthenticationFilter() throws Exception {
+        AuthenticationFilter authenticationFilter = new AuthenticationFilter();
+        authenticationFilter.setAuthenticationManager(authenticationManager());
+        return authenticationFilter;
     }
 }
