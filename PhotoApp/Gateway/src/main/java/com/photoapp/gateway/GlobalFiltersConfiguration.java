e67b5ffe36b392d2a734a147c34596af84f44d36
Yan Reginevich <83975655+BonGeras@users.noreply.github.com>
2023-02-15 21:48:13 +0100

    Ordering global filters

--- PhotoApp/Gateway/src/main/java/com/photoapp/gateway/GlobalFiltersConfiguration.java
+++ PhotoApp/Gateway/src/main/java/com/photoapp/gateway/GlobalFiltersConfiguration.java
@@ -5,12 +5,14 @@ import org.slf4j.LoggerFactory;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import reactor.core.publisher.Mono;
 
 @Configuration
 public class GlobalFiltersConfiguration {
     Logger logger = LoggerFactory.getLogger(GlobalFiltersConfiguration.class);
 
+    @Order(1)
     @Bean
     public GlobalFilter secondPreFilter() {
         return ((exchange, chain) -> {
@@ -21,6 +23,7 @@ public class GlobalFiltersConfiguration {
         });
     }
 
+    @Order(2)
     @Bean
     public GlobalFilter thirdPreFilter() {
         return ((exchange, chain) -> {
@@ -31,6 +34,7 @@ public class GlobalFiltersConfiguration {
         });
     }
 
+    @Order(3)
     @Bean
     public GlobalFilter fourthPreFilter() {
         return ((exchange, chain) -> {
